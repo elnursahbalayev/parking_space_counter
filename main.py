b3e9cd5c316550725cbd702e959f674ec1a788b1
Elnur Shahbalayev <elnursahbalayev@gmail.com>
2025-05-20 08:27:45 +0400

    feat(main): add parking spot detection and counting
    - Implement parking spot detection using a trained model
    - Add counting of available parking spots
    - Display spot status and availability count on the video frame- Introduce util.py module for spot detection and image processing

--- main.py
+++ main.py
@@ -35,4 +35,48 @@ while ret:
 
             spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
 
+            diffs[spot_indx] = calc_diff(spot_crop, previous_frame[y1:y1+h, x1:x1+w, :])
 
+        print([diffs[j] for j in np.argsort(diffs)][::-1])
+
+    if frame_nmr % step == 0:
+        if previous_frame is None:
+            arr_ = range(len(spots))
+        else:
+            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) < 0.4]
+
+        for spot_indx in arr_:
+            spot = spots[spot_indx]
+            x1, y1, w, h = spot
+
+            spot_crop = frame[y1:y1+h, x1:x1+w, :]
+
+            spots_status = empty_or_not(spot_crop)
+
+            spots_status[spot_indx] = spots_status
+
+    if frame_nmr % step == 0:
+        previous_frame = frame.copy()
+
+    for spot_indx, spot in enumerate(spots):
+        spot_status = spots_status[spot_indx]
+        x1, y1, w, h = spots[spot_indx]
+
+        if spot_status:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+        else:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+
+    cv2.rectangle(frame, (80, 20), (550, 80), (0, 0, 0), -1)
+    cv2.putText(frame, f'Available spots: {str(sum(spots_status))} / {str(len(spots_status))}', (100, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    frame_nmr += 1
+
+cap.release()
+cv2.destroyAllWindows()
